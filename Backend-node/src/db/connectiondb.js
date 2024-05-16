1e86ff75a0feffc79261749f290b0c857b895c4a
Duilia Guerrero <duilia.guerrero@gmail.com>
2024-05-15 19:15:03 -0500

    vercel bulds update1

--- Backend-node/src/db/connectiondb.js
+++ Backend-node/src/db/connectiondb.js
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('login_jwt', 'root', '1234root', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: "mysql12"
   });
 
 export default db;
